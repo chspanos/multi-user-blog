504dc67432cbb749501304b38b22ac9a0c42e478
Cheryl Spanos <chspanos@comcast.net>
2017-04-26 17:21:08 -0700

    doc: Add comments and code clean up
    Add comments and code clean up to meet Python style guidelines

--- blog.py
+++ blog.py
@@ -1,3 +1,18 @@
+# Program to create a multi-user blog implemented with Google App Engine
+# and Jinja templates.
+#
+# Dependencies: app.yaml
+#   templates/blog-base.html
+#   templates/front.html
+#   templates/welcome.html
+#   templates/permalink.html
+#   templates/form.html
+#   templates/comment.html
+#   templates/login-base.html
+#   templates/signup.html
+#   templates/login.html
+#   static/css/blog-style.css
+#   static/css/login-style.css
 import os
 import re
 import random
@@ -15,7 +30,8 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                 autoescape = True)
 
-# Helper functions for verifying input
+
+# Helper functions for verifying signup and login input
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
     return username and USER_RE.match(username)
@@ -27,6 +43,7 @@ def valid_password(password):
 EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
 def valid_email(email):
     return not email or EMAIL_RE.match(email)
+
 
 # Helper functions for hashing passwords
 def make_salt():
@@ -41,6 +58,7 @@ def make_pw_hash(name, pw, salt=None):
 def valid_pw(name, pw, h):
     salt = h.split(',')[1]
     return h == make_pw_hash(name, pw, salt)
+
 
 # Helper functions for creating cookies
 SECRET = 'Really, really, super secret string'
@@ -57,111 +75,144 @@ def check_secure_cookie(h):
 
 # Create our User registry database
 class User(db.Model):
+    """ User class for our blog registry database
+
+    Attributes:
+        name - username (string, required)
+        hashed_pw - hashed password (string, required)
+        email - email address (string, optional)
+    """
     name = db.StringProperty(required = True)
     hashed_pw = db.StringProperty(required = True)
     email = db.StringProperty()
 
     @classmethod
     def by_name(cls, name):
+        """ Searches the User database for a given username and
+        returns the first matching entry
+        """
         return cls.all().filter('name =', name).get()
 
 
 # Create our Blog database
 class BlogPost(db.Model):
+    """ BlogPost class for representing blog entries
+
+    Attributes:
+        author - post author (reference to User object, required)
+        subject - blog subject line (string, required)
+        content - blog content (text block, required)
+        created - date created (date/time, automatically generated)
+        likes - list of users who liked the post (list of user_ids (int))
+        comments - list of comments (list of comment_ids (int))
+    """
     author = db.ReferenceProperty(User)
     subject = db.StringProperty(required = True)
     content = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now = True)
     likes = db.ListProperty(int, default=None)
     comments = db.ListProperty(int, default=None)
 
     def get_id(self):
+        """ Returns blog id """
         return self.key().id()
 
     def valid_author(self, user):
-        ''' Returns True if this is a valid user and the author of
-        this post. '''
+        """ Returns True if this is a valid user and the author of
+        this post.
+        """
         return user and self.author.name == user.name
 
     def update_post_content(self, subject, content):
-        ''' Updates post subject and content fields. Note: You still
-        need to put() to update database. '''
+        """ Updates post subject and content fields.  Note: You still
+        need to put() to update database.
+        """
         self.subject = subject
         self.content = content
 
     def user_already_liked(self, user_id):
-        '''Returns True if this user already liked this post.'''
+        """ Returns True if this user already liked this post """
         return int(user_id) in self.likes
 
     def add_like(self, user_id):
-        ''' Add a like from this user '''
+        """ Add a like from this user """
         self.likes.append(int(user_id))
 
     def like_count(self):
-        ''' Returns number of likes. '''
+        """ Returns number of likes """
         return len(self.likes)
 
     def add_comment(self, cid):
-        ''' Adds a comment id to the comments list '''
+        """ Adds a comment id to the comments list """
         self.comments.append(cid)
 
     def comment_count(self):
-        ''' Returns number of comments '''
+        """ Returns number of comments """
         return len(self.comments)
 
     def get_comment(self, cid):
-        ''' Returns comment text '''
+        """ Returns comment text """
         return Comment.get_text_by_id(cid)
 
     def get_comment_author(self, cid):
-        ''' Returns comment author '''
+        """ Returns comment author """
         return Comment.get_author_by_id(cid)
 
     def delete_comment(self, cid):
-        ''' Deletes this comment id from the comments list '''
+        """ Deletes this comment id from the comments list """
         self.comments.remove(cid)
 
 
 # Create our comment database
 class Comment(db.Model):
+    """ Comment class for storing user comments.
+
+    Attributes:
+        blog_post - blog post associated with this comment (reference to
+            a BlogPost object, required)
+        author - comment author (reference to a User object, required)
+        text - comment text (text block, required)
+        created - date created (date/time, automatically generated)
+    """
     blog_post = db.ReferenceProperty(BlogPost)
     author = db.ReferenceProperty(User)
     text = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now = True)
 
     @classmethod
     def get_text_by_id(cls, cid):
-        ''' Looks up comment by id and returns text '''
+        """ Looks up comment by id and returns its text """
         comment = cid and cls.get_by_id(cid)
         return comment and comment.text
 
     @classmethod
     def get_author_by_id(cls, cid):
-        ''' Looks up comment by id and returns author name'''
+        """ Looks up comment by id and returns its author's username """
         comment = cid and cls.get_by_id(cid)
         user = comment and comment.author
         return user and user.name
 
     def valid_author(self, user):
-        ''' Returns True if this is a valid user and the author of
-        this comment. '''
+        """ Returns True if this is a valid user and the author of
+        this comment. """
         return user and self.author.name == user.name
 
     def update_text(self, text):
-        ''' Updates comment text field. Note: You still
-        need to put() to update database. '''
+        """ Updates comment text attribute. Note: You still
+        need to put() to update database. """
         self.text = text
 
 
 # Page Handlers
 # Parent page handler
 class Handler(webapp2.RequestHandler):
+    """ Parent page handler class """
     def write(self, *a, **kw):
+        """ Boilerplate write method """
         self.response.write(*a, **kw)
 
     def render_str(self, template, **params):
+        """ Boilerplate render_str method """
         # add user to the parameter list automatically
         params['user'] = self.user
         # set template and call render like before
@@ -169,31 +220,36 @@ class Handler(webapp2.RequestHandler):
         return t.render(params)
 
     def render(self, template, **kw):
+        """ Boilerplace render method """
         self.write(self.render_str(template, **kw))
 
     def get_uid_from_cookie(self):
-        ''' Get user_id from cookie and check if valid. Returns valid id. '''
+        """ Get user_id from cookie and check if valid. Returns valid id. """
         user_id = self.request.cookies.get('user_id')
         return user_id and check_secure_cookie(user_id)
 
     def initialize(self, *a, **kw):
-        ''' Overwrite Handler initialize method to also check for and
-        load user info from cookie '''
+        """ Overwrites standard initialize method to also check for and
+        load user info from cookie
+        """
         webapp2.RequestHandler.initialize(self, *a, **kw)
         uid = self.get_uid_from_cookie()
         self.user = uid and User.get_by_id(int(uid))
 
     def get_post_id(self):
-        ''' Queries page input for blog_id and returns it as an integer. '''
+        """ Queries page input for blog_id and returns it as an integer. """
         return int(self.request.get('blog_id'))
 
     def get_comment_id(self):
-        ''' Queries page input for comment id and returns it as an int. '''
+        """ Queries page input for comment id and returns it as an int. """
         return int(self.request.get('cid'))
 
 
 # Signup page
 class SignupHandler(Handler):
+    """ Signup page handler loads signup HTML template and processes
+    user data to create a new User in the registry
+    """
     def get(self):
         self.render('signup.html')
 
@@ -210,7 +266,6 @@ class SignupHandler(Handler):
         passwd_error = ""
         verify_error = ""
         email_error = ""
-
         if not valid_username(username):
             user_error = "That's not a valid username"
             input_error = True
@@ -225,10 +280,9 @@ class SignupHandler(Handler):
             input_error = True
 
         # Check if this user is already in our registry
-        if not input_error:
-            if User.by_name(username):
-                user_error = "The user already exists"
-                input_error = True
+        if not input_error and User.by_name(username):
+            user_error = "The user already exists"
+            input_error = True
 
         if not input_error:
             # Create user entry in database
@@ -250,6 +304,9 @@ class SignupHandler(Handler):
 
 # Login page
 class LoginHandler(Handler):
+    """ Login page handler loads login HTML template and processes user
+    data to retrieve an existing User from the registry.
+    """
     def get(self):
         self.render('login.html')
 
@@ -257,7 +314,6 @@ class LoginHandler(Handler):
         # Get user input
         username = self.request.get('username')
         password = self.request.get('password')
-
         # Check if user is in database
         u = User.by_name(username)
         if u and valid_pw(username, password, u.hashed_pw):
@@ -277,6 +333,7 @@ class LoginHandler(Handler):
 
 # Logout page
 class LogoutHandler(Handler):
+    """ Logout handler to sign a registered User out of the blog """
     def get(self):
         # Clear cookies
         self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
@@ -286,6 +343,11 @@ class LogoutHandler(Handler):
 
 # Welcome page
 class WelcomeHandler(Handler):
+    """ Welcome page handler loads the welcome HTML template. This page
+    displays a welcome message and control panel for this User. It
+    contains a table of all the user's posts and allows them to create,
+    edit, or delete their posts.
+    """
     def get(self):
         # if user is logged in
         if self.user:
@@ -298,6 +360,10 @@ class WelcomeHandler(Handler):
 
 # Front Blog page
 class MainPage(Handler):
+    """ Main page handler for the blog loads the front HTML template.
+    This page displays the most recent 10 blog posts with links to their
+    corresponding permalink pages.
+    """
     def get(self):
         # Get blog entries
         entries = db.GqlQuery("SELECT * FROM BlogPost "
@@ -307,6 +373,9 @@ class MainPage(Handler):
 
 # New blog post entry page
 class NewPost(Handler):
+    """ NewPost page handler loads the form HTML template and processes
+    User input to create a new blog post and add it to the database.
+    """
     def get(self):
         self.render('form.html')
 
@@ -320,7 +389,8 @@ class NewPost(Handler):
                 # Error checking on input
                 if subject and content:
                     # Create new Blog Post
-                    b = BlogPost(author=self.user, subject=subject, content=content)
+                    b = BlogPost(author=self.user, subject=subject,
+                                content=content)
                     b.put()
                     # Redirect to permalink page
                     blog_id = b.key().id()
@@ -340,6 +410,9 @@ class NewPost(Handler):
 
 # Edit blog post page
 class EditPost(Handler):
+    """ EditPost handler reloads the form HTML template with the contents
+    of a previous blog post entry and allows the User to edit its contents.
+    """
     def get(self):
         # Get post from id
         blog_id = self.get_post_id()
@@ -404,6 +477,9 @@ class EditPost(Handler):
 
 # Delete post handler
 class DeletePost(Handler):
+    """ DeletePost page handler process a User request to delete their post
+    from the blog database.
+    """
     def get(self):
         # Get post from id
         blog_id = self.get_post_id()
@@ -428,12 +504,20 @@ class DeletePost(Handler):
 
 # Permalink blog page
 class PermalinkHandler(Handler):
+    """ Permalink page handler loads the permalink HTML template which
+    displays a single blog post, including the blog content,
+    number of likes, and all associated comments. It also allows
+    registered users to like this post and create, edit, and delete
+    user-owned comments.
+    """
     def get(self, blog_id):
         entry = BlogPost.get_by_id(int(blog_id))
         self.render('permalink.html', entry=entry)
 
+
 # Like post handler
 class LikeHandler(Handler):
+    """ Like handler processes a user request to "like" a blog post """
     def get(self, blog_id):
         # get blog entry
         entry = BlogPost.get_by_id(int(blog_id))
@@ -455,8 +539,12 @@ class LikeHandler(Handler):
             self.render('permalink.html', entry=entry)
 
 
-# New comment page handler
+# New comment page
 class NewComment(Handler):
+    """ New Comment handler loads the comment HTML template and
+    processes user input to create a new comment. The new comment is
+    added to the Comment database and linked with the blog post.
+    """
     def get(self, blog_id):
         # Get post from id
         blog_post = BlogPost.get_by_id(int(blog_id))
@@ -474,7 +562,8 @@ class NewComment(Handler):
                 comment = self.request.get('comment')
                 if comment:
                     # Create new Comment object
-                    c = Comment(blog_post=blog_post, author=self.user, text=comment)
+                    c = Comment(blog_post=blog_post, author=self.user,
+                                text=comment)
                     c.put()
                     # Add comment to blog_post
                     cid = c.key().id()
@@ -497,6 +586,9 @@ class NewComment(Handler):
 
 # Edit comment page handler
 class EditComment(Handler):
+    """ EditComment handler reloads the comment HTML template with the
+    current comment text and allows its User author to edit it.
+    """
     def get(self, blog_id):
         # Get post from id
         blog_post = BlogPost.get_by_id(int(blog_id))
@@ -552,6 +644,10 @@ class EditComment(Handler):
 
 # Delete comment handler
 class DeleteComment(Handler):
+    """ DeleteComment handler processes a User request to remove a
+    comment that they made from the blog and delete it from the
+    comment database.
+    """
     def get(self, blog_id):
         # Get post from id
         blog_post = BlogPost.get_by_id(int(blog_id))
